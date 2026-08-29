1939ab43e6eada29713e6315d8b2e297d23a9540
Alexey Ganzha <aganzha@yandex.ru>
2026-08-29 20:19:53 +0200

    works with timeout but not within emacs copying itself. perhaps event this is enough

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 clear 
-cargo build --lib --release 
 export RUST_BACKTRACE=1
 export NO_ALPHA=1
-emacs
+cargo build --lib --release && emacs
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use core::time::Duration;
 use emacs::{Env, Result, Value, defun};
+use gtk::{gdk, glib};
 use std::sync::OnceLock;
+
+//use emacs::use_symbols;
 emacs::plugin_is_GPL_compatible!();
-use emacs::use_symbols;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -18,6 +21,21 @@ fn init<'a>(env: &'a Env) -> Result<Value<'a>> {
         return env.intern("t");
     }
     let _ = env.message("🐦 live-ring: gtk initialized");
+    if let Some(display) = gdk::Display::default() {
+        eprintln!("💦 ...............{}", display);
+        if let Some(clipboard) = gtk::Clipboard::default(&display) {
+            println!("🧶 ...............{}", clipboard);
+            glib::timeout_add_local(Duration::from_millis(1000), {
+                move || {
+                    eprintln!("🦴 loop");
+                    if let Some(text) = clipboard.wait_for_text() {
+                        eprintln!("🌻 ...............{}", text);
+                    }
+                    glib::ControlFlow::Continue
+                }
+            });
+        }
+    }
     env.intern("t")
 }
 
